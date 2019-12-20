007badd691c3e05d33df917d9b105105afd8c102
sreeneher3913 <55245727+sreeneher3913@users.noreply.github.com>
2019-12-20 15:25:45 +0530

    Change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
 app.get('/login', (req, res) => res.send('Login'))
 app.get('/register', (req, res) => res.send('Register'))
 
-var student = {"name" : "Being 0" , "cllge" : "Vnrvjiet" , "regno" : "17071A1252"}
+var student = {"name" : "Sree Neher" , "cllge" : "Vnrvjiet" , "regno" : "17071A1252"}
 app.get('/data',(req,res) => res.json(student))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
